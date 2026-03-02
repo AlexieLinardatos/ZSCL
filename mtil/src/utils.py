e019f9df9d191805850f076d343dd691548a000a
AlexieLinardatos <alexie.linardatos@gmail.com>
2026-03-02 15:55:40 -0500

    Fix accuracy scalar conversion for eval on newer numpy/torch

--- mtil/src/utils.py
+++ mtil/src/utils.py
@@ -36,7 +36,7 @@ def accuracy(output, target, topk=(1,)):
     pred = output.topk(max(topk), 1, True, True)[1].t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
     return [
-        float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy())
+        correct[:k].reshape(-1).float().sum().item()
         for k in topk
     ]
 
